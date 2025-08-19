a8b832040b7ca03fee54e0dcf7b4f1a6ec162a34
Matheus Cesar Reis <cematheus8@gmail.com>
2025-08-18 23:11:45 -0300

    Adição do log na calculadora

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -59,4 +59,10 @@ public class Calculadora {
         if (n == 0 || n == 1) return 1;
         return n * fatorial(n - 1);
     }
+
+    public double logBase() {
+        if (this.a <= 0) throw new ArithmeticException("Logaritmo não definido para números <= 0!");
+        if (this.b <= 0 || this.b == 1) throw new ArithmeticException("Base do logaritmo deve ser positiva e diferente de 1!");
+        return Math.log(this.a) / Math.log(this.b);
+    }
 }
